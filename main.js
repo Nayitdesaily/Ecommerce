dd7866996a4ff6831c38771fde8d71b9c2024e14
Nayitdesaily <nayit.ruizj@gmail.com>
2022-06-16 11:08:44 -0500

    Arrglo de main.js en el total

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 "use strict"
 
 
+
 /*Header*/
 /*Menu Lateral Hamburguer*/
 
 const hamburguer = document.getElementById("hamburguer")
-const lateralBar = document.querySelector(".lateral-bar")
+const lateralBar = document.getElementById("lateral-bar")
 const cerrar = document.getElementById("cerrar")
 
-hamburguer.addEventListener("click", () =>{
-    lateralBar.classList.remove("active")
-});
-cerrar.addEventListener("click", () =>{
-    lateralBar.classList.add("active")
-});
+if(hamburguer){
+    hamburguer.addEventListener("click", () => {
+        lateralBar.classList.add("active")
+    })
+}
 
-
+if(cerrar){
+    cerrar.addEventListener("click", () => {
+        lateralBar.classList.remove("active")
+    })
+}
 /*Menu Login */
 const login = document.getElementById("login");
 const user = document.getElementById("user");
@@ -33,11 +37,60 @@ const cart = document.querySelector(".cart")
 const closeCart = document.getElementById("close-cart");
 
 shopping.addEventListener("click", () =>{
-    cart.classList.remove("cart-active")
-})
-closeCart.addEventListener("click", () =>{
     cart.classList.add("cart-active")
 })
+closeCart.addEventListener("click", () =>{
+    cart.classList.remove("cart-active")
+})
+
+/*Shop Working */
+/*Remove Item From Cart */
+let removeCartButton = document.getElementsByClassName("cart-remove")
+console.log(removeCartButton)
+for (let i=0; i<removeCartButton.length; i++){
+    let button = removeCartButton[i]
+    button.addEventListener("click", removeCardItem)
+}
+
+/*Quantity Changes*/
+let quantityValues = document.getElementsByClassName("cart-quantity")
+for (let i=0; i<quantityValues.length; i++){
+    let value = quantityValues[i]
+    value.addEventListener ("change", quantityChanged)
+}
+
+/*Remove Item */
+function removeCardItem (event){
+    let buttonClicked = event.target
+    buttonClicked.parentElement.remove();
+}
+
+function quantityChanged (event){
+    let input = event.target
+    if( input.value == 0 || input.value == NaN){
+        input.value = 1
+    }
+    updateTotal();
+}
+
+
+
+/*Update Total */
+function updateTotal (){
+    let cartContent = document.getElementsByClassName("cart-content")[0]
+    let cartBoxes = cartContent.getElementsByClassName("cart-box")
+    let total = 0
+    for (let i=0; i<cartBoxes.length; i++){
+        let cartBox = cartBoxes[i]
+        let priceProduct = cartBox.getElementsByClassName("cart-price") [0];
+        let price = parseFloat (priceProduct.innerText.replace("$",""));
+        let quantityProduct = cartBox.getElementsByClassName("cart-quantity")[0];
+        let quantity = quantityProduct.value;
+        total = total + price * quantity;
+    }
+        document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+    
+}
 
 
 
@@ -108,7 +161,7 @@ smartphone.forEach ( (producto) => {
                 </div>
                 <div>
                 <small>Precio</small>
-                <h3 class="precio-product">${"$" + producto.precio}</h3>
+                <h3 class="precio-product">${producto.precio}</h3>
                 </div>
                 <img src="./ASSETS/Group 56.svg" alt="" class="icon-card">
             </div>
@@ -118,26 +171,7 @@ smartphone.forEach ( (producto) => {
 contenedor.innerHTML = fragmentoInnerHTML
 
 
-/*Shop Working */
-
-/*Remover Item de Carrito */
-let removeCartButton = document.getElementsByClassName("cart-remove")
-
-for (let i=0; i<removeCartButton.length; i++){
-    let button = removeCartButton[i]
-    button.addEventListener("click", removeCardItem)
-}
-
-
-/*Cambio de cantidad*/
-let quantityValues = document.getElementsByClassName("cart-quantity")
-for (let i=0; i<quantityValues.length; i++){
-    let value = quantityValues[i]
-    value.addEventListener ("change", quantityChanged)
-}
-
-
-/*Agregar funcion a boton de compra*/
+/*Add Card*/
 let agregarCard = document.getElementsByClassName("icon-card")
 for(let i=0; i<agregarCard.length; i++){
     let buttonAgregarCard = agregarCard[i];
@@ -147,46 +181,6 @@ for(let i=0; i<agregarCard.length; i++){
 
 /*Boton Proceder al pago */
 document.getElementsByClassName("btn-buy")[0].addEventListener("click", btnBuyClicked)
-
-
-
-
-/*Remover Item */
-function removeCardItem (event){
-    let buttonClicked = event.target
-    buttonClicked.parentElement.remove();
-    
-}
-
-function quantityChanged (event){
-    let input = event.target
-    if( input.value == 0 || input.value == NaN){
-        input.value = 1
-    }
-    updateTotal();
-}
-
-
-
-/*Actualizar Total */
-function updateTotal (){
-    let cartContent = document.getElementsByClassName("cart-content")[0]
-    let cartBoxes = cartContent.getElementsByClassName("cart-box")
-    let total = 0
-    for (let i=0; i<cartBoxes.length; i++){
-        let cartBox = cartBoxes[i]
-        let priceProduct = cartBox.getElementsByClassName("cart-price") [0];
-        let price = parseFloat (priceProduct.innerText.replace("$",""));
-        let quantityProduct = cartBox.getElementsByClassName("cart-quantity")[0];
-        let quantity = quantityProduct.value;
-        total = total + price * quantity;
-    }
-        document.getElementsByClassName("total-price")[0].innerText = "$" + total;
-    
-}
-
-
-
 
 function btnBuyClicked (event) {
     let buttonBuy = event.target
@@ -202,7 +196,7 @@ function btnBuyClicked (event) {
 
 
 
-/*Agregar a carrito */
+/*Add to Carts */
 function agregarCardClicked (event) {
     let buttonAgregarCard = event.target
     let choosenProduct = buttonAgregarCard.parentElement
@@ -242,115 +236,3 @@ cartItem.append(cartShopBox);
 cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCardItem)
 cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged)
 }
-
-/*Local Storage */
-
-/*
-let addCardBtn = document.getElementsByClassName("icon-card");
-let items = [ ];
-for (let i=0; i<addCardBtn.length; i++){
-    addCardBtn[i].addEventListener("click", function(e){
-        if(typeof(Storage) !== undefined){
-            let item = {
-                id: i,
-                name: e.target.parentElement.children[2].children[1].textContent,
-                price: e.target.parentElement.children[3].children[1].textContent,
-                no: 1
-                };
-            if(JSON.parse(localStorage.getItem("newlist") === null)){
-                items.push(item);
-                localStorage.setItem("newlist", JSON.stringify(items));
-                
-            }else {
-                let localItems = JSON.parse(localStorage.getItem("newlist"));
-                localItems.map(data =>{
-                    if(item.id == data.id){
-                        item.no = data.no + 1
-                    }else {
-                        items.push(data)
-                    }
-                });
-                items.push(item)
-                localStorage.setItem("newlist", JSON.stringify(items));
-                
-            }
-        }else{  
-            alert("storage is not working")
-        }
-    })
-}*/
-
-/*let numberShopCart = document.getElementsByClassName("number-shopping")
-let no = 0;
-JSON.parse(localStorage.getItem("items")).map(data =>{
-    no = no + data.no
-})
-numberShopCart.innerHTML = no*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function saveLocalStorage(){
-    let cartContentStorage = document.querySelector(".cart-content")
-    localStorage.setItem("list", JSON.stringify(cartContentStorage.outerHTML))
-    
-    }
-    
-    function loadLocalStorage(){
-   console.log  (JSON.parse(localStorage.getItem("list")))
-    }*/
-    /*document.addEventListener("DOMContentLoaded", () =>{
-        savelocalstorage()
-        loadlocalstorage()
-    })
-      function savelocalstorage(){
-        let cartContentList = document.getElementById("itemstorage");
-        localStorage.setItem("list", JSON.stringify(cartContentList.outerHTML));
-      }
-
-      function loadlocalstorage(){
-        console.log(JSON.parse(localStorage.getItem("list")));
-      }*/
-      
-/*
-//let likeBtn = document.querySelector("#btn-like")
-//let small = document.querySelector("small")
-
-
-    if( window.localStorage.getItem ( "myStorage")  ){
-        contadorLike = JSON.parse( window.localStorage.getItem ( "myStorage") )
-        small.textContent = `Likes ${contadorLike} `
-
-        likeBtn.addEventListener( "click", (eventoData) =>{
-            contadorLike++
-            small.textContent = `Likes ${contadorLike} `
-            window.localStorage.setItem( "myStorage", JSON.stringify( contadorLike ) )
-        })
-    }else{
-        contadorLike = 0
-        likeBtn.addEventListener( "click", (eventoData) =>{
-        contadorLike++
-        small.textContent = `Likes ${contadorLike} `
-        let myStorage = "Likes"
-        window.localStorage.setItem( "myStorage", JSON.stringify( contadorLike ) )
-        let value = window.localStorage.getItem ( "myStorage")
-        console.log ( value )
-        })
-}
-*/
